b0d1e89e5f955520bd02eb90b78e7c59e08bca74
akhil <akhil@pop-os.localdomain>
2022-12-25 11:15:26 +0530

    updated the initialization script for jupyter

--- rootfs/etc/ipython/ipython_config.py
+++ rootfs/etc/ipython/ipython_config.py
@@ -1,19 +1,30 @@
 c.InteractiveShellApp.exec_lines = [
-    "from os import environ",
-    "from subprocess import getoutput",
-    "from pyspark import SparkConf",
-    "from pyspark import SparkContext",
-    """for i in environ.keys():
+    """
+from os import environ
+from pyspark.sql import SparkSession
+from subprocess import getoutput
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+for i in environ.keys():
     if len(i)-i.find("MASTER_SVC_PORT")==15 and environ[i].find("7077")>=0:
         master=environ[i].replace("tcp","spark")
-        break""",
-    "conf = SparkConf()",
-    """conf.setMaster(master)""",
-    """conf.set('spark.driver.host',getoutput('hostname -I').strip())""",
-    """conf.set('spark.jars.ivy',"/tmp/.ivy2")""",
-    """if sc:
-    sc.stop()""",
-    """if spark:
-    spark.stop()""",
-    "sc=SparkContext(conf=conf)"
+        break
+
+conf = SparkConf()
+conf.setMaster(master)
+conf.set('spark.driver.host',getoutput('hostname -I').strip())
+conf.set('spark.jars.ivy',"./.ivy2")
+
+try:
+    if sc:
+        sc.stop()
+    if spark:
+        spark.stop()
+except:
+    pass
+
+sc=SparkContext(conf=conf)
+spark = SparkSession(sc)
+"""
 ]
